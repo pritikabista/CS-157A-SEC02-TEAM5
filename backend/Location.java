38947812adc049fa396c5387ac00e2f3a1b87edf
Dee <aeinhmu.naychi@sjsu.edu>
2026-03-17 18:20:35 -0700

    Implements Location class

---
+++ backend/Location.java
@@ -0,0 +1,28 @@
+package backend;
+
+public class Location {
+
+    private int locationID;
+    private int roomNumber;
+    private int buildingNumber;
+
+    public Location() {
+        this.locationID = 0;
+        this.roomNumber = 0;
+        this.buildingNumber = 0;
+    }
+
+    public Location(int locationID, int roomNumber, int buildingNumber) {
+        this.locationID = locationID;
+        this.roomNumber = roomNumber;
+        this.buildingNumber = buildingNumber;
+    }
+
+    void setLocationID(int locationID) { this.locationID = locationID; }
+    void setRoomNumber(int roomNumber) { this.roomNumber = roomNumber; }
+    void setBuildingNumber(int buildingNumber) { this.buildingNumber = buildingNumber; }
+
+    int getLocationID() { return this.locationID; }
+    int getRoomNumber() { return this.roomNumber; }
+    int getBuildingNumber() { return this.buildingNumber; }
+}
