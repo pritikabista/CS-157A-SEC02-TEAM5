1f6019dd229f8353ca9115f6855365ab52a648fa
Dee <aeinhmu.naychi@sjsu.edu>
2026-03-17 18:07:06 -0700

    Implement subclass Admin that extends Account

---
+++ backend/Admin.java
@@ -0,0 +1,24 @@
+package backend;
+
+public class Admin extends Account{
+    private int departmentID;
+    private int officeNum; 
+
+    public Admin(){
+        super();
+        this.departmentID = 0;
+        this.officeNum = 0;
+    }
+
+    public Admin(int accountID, String username, String pwdHashed, int departmentID, int officeNum){
+        super(accountID, username, pwdHashed);
+        this.departmentID = departmentID;
+        this.officeNum = officeNum;
+    }
+
+    void setDepartmentID(int departmentID) { this.departmentID = departmentID; }
+    void setOfficeNum(int officeNum) { this.officeNum = officeNum; }
+
+    int getDepartmentID() { return this.departmentID; }
+    int getOfficeNum() { return this.officeNum; }
+}
