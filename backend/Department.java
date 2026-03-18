569a5ca6a150e7a0e457ade082f3f868abcd4ca7
Dee <aeinhmu.naychi@sjsu.edu>
2026-03-17 18:10:53 -0700

    Implements draft version of Department that users and admins will belong to

---
+++ backend/Department.java
@@ -0,0 +1,23 @@
+package backend;
+
+public class Department {
+    private int departmentID;
+    private String departmentName; 
+
+    public Department(){
+        this.departmentID = 0;
+        this.departmentName = null;
+    }
+
+    public Department(int departmentID, String departmentName){
+        this.departmentID = departmentID;
+        this.departmentName = departmentName; 
+    }
+
+    void setDepartmentID(int departmentID) { this.departmentID = departmentID; }
+    void setDepartmentName(String departmentName) { this.departmentName = departmentName; }
+
+    int getDepartmentID() { return this.departmentID; }
+    String getDepartmentName() { return this.departmentName; }
+    
+}
