a80298780d4cd883156afd0b86b5804be51f234a
Dee <aeinhmu.naychi@sjsu.edu>
2026-03-17 18:30:11 -0700

    Implement Supplier class

---
+++ backend/Supplier.java
@@ -0,0 +1,28 @@
+package backend;
+
+public class Supplier {
+
+    private int supplierID;
+    private String phNum;
+    private String url;
+
+    public Supplier() {
+        this.supplierID = 0;
+        this.phNum = "";
+        this.url = "";
+    }
+
+    public Supplier(int supplierID, String phNum, String url) {
+        this.supplierID = supplierID;
+        this.phNum = phNum;
+        this.url = url;
+    }
+
+    void setSupplierID(int supplierID) { this.supplierID = supplierID; }
+    void setPhNum(String phNum) { this.phNum = phNum; }
+    void setUrl(String url) { this.url = url; }
+
+    int getSupplierID() { return this.supplierID; }
+    String getPhNum() { return this.phNum; }
+    String getUrl() { return this.url; }
+}
