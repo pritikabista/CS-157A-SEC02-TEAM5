e3d62ca3de8187dceaaf6ea675e792660a8ac5ae
Dee <aeinhmu.naychi@sjsu.edu>
2026-03-17 18:24:27 -0700

    Implements draft version of Category class

---
+++ backend/Category.java
@@ -0,0 +1,23 @@
+package backend;
+
+public class Category {
+
+    private int categoryID;
+    private String categoryName;
+
+    public Category() {
+        this.categoryID = 0;
+        this.categoryName = "";
+    }
+
+    public Category(int categoryID, String categoryName) {
+        this.categoryID = categoryID;
+        this.categoryName = categoryName;
+    }
+
+    void setCategoryID(int categoryID) { this.categoryID = categoryID; }
+    void setCategoryName(String categoryName) { this.categoryName = categoryName; }
+
+    int getCategoryID() { return this.categoryID; }
+    String getCategoryName() { return this.categoryName; }
+}
