363b76c816ed8d2baef9ab1e852757ba7ebac045
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-17 23:36:36 -0700

    Update Login with DAO classes

--- src/main/java/com/medicalims/servlet/LoginServlet.java
+++ src/main/java/com/medicalims/servlet/LoginServlet.java
@@ -4,25 +4,74 @@ import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
-import com.medicalims.util.DBConnection;
-
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
+import com.medicalims.database.*;
+import com.medicalims.model.*;
+import com.medicalims.util.HashPw;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 
     @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) //this request is sending data to the server to be processed
             throws ServletException, IOException {
 
         String username = request.getParameter("username");
         String password = request.getParameter("password");
 
-        String sql = "SELECT * FROM accounts WHERE username = ? AND password = ?";
+        //invalid username or password -> return early
+        if (password == null || password.isEmpty() || username == null || username.isEmpty()){
+            request.setAttribute("error", "Username or Password cannot be null!");
+            request.getRequestDispatcher("/pages/login.jsp").forward(request, response);
+            return; 
+        }
 
+        AccountDAO accountDAO = new AccountDAO(); 
+        Account currAccount = accountDAO.getAccountIfExist(username); //get the account with the username (assuming usernames are unique)
+
+        if (currAccount == null){ //no account with that username
+            request.setAttribute("error", "No such account exists!");
+            request.getRequestDispatcher("/pages/login.jsp").forward(request, response);
+            return; 
+        }
+
+        String hashedPwdFromDatabase = currAccount.getPwdHashed(); //get the account's hashedPwd stored in Database
+        if (!HashPw.checkPwd(password, hashedPwdFromDatabase)){ //wrong password
+            request.setAttribute("error", "Invalid Username or Password!"); // :)
+            request.getRequestDispatcher("/pages/login.jsp").forward(request, response);
+            return; 
+        }
+
+        //Check if the current account is user or admin
+        UserDAO userDAO = new UserDAO();
+        User currUser = userDAO.getUserIfExist(currAccount.getAccountID());
+        boolean isAdmin = false;
+
+        Admin currAdmin = null; 
+
+        if (currUser == null){ //rule: every verified account is either a user or an admin
+            AdminDAO adminDAO = new AdminDAO();
+            currAdmin = adminDAO.getAdminIfExist(currAccount.getAccountID());
+            if (currAdmin != null) {isAdmin = true;}
+        }
+
+        HttpSession session = request.getSession(); //create session for the logged-in browser or gets the existing one 
+        
+        if (isAdmin){ //*****! new attribute added here, this is an Admin Object, see Admin class *****!/
+            session.setAttribute("admin", currAdmin); 
+            session.setAttribute("role", "admin");
+        }
+        else{
+            session.setAttribute("user", currUser);
+            session.setAttribute("role", "user");
+        }
+        
+        session.setAttribute("username", currAccount.getUsername());
+        response.sendRedirect("/pages/dashboard.jsp");
+    
+        
+        /* 
         try (Connection conn = DBConnection.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
 
@@ -45,5 +94,6 @@ public class LoginServlet extends HttpServlet {
         } catch (Exception e) {
             throw new ServletException(e);
         }
+        */
     }
 }
