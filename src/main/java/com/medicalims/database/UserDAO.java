de7864bb4c1d07deba22e91ddc09492dd8b0eee1
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-18 00:49:31 -0700

    Update UserDAO with DBConnection class and insertUser to add to Users table

--- src/main/java/com/medicalims/database/UserDAO.java
+++ src/main/java/com/medicalims/database/UserDAO.java
@@ -8,14 +8,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.medicalims.model.User;
+import com.medicalims.util.*;
 
 public class UserDAO {
-    private final String url = "jdbc:mysql://localhost:3306/team5?useSSL=false&serverTimezone=UTC"; 
-    private final String user = "root";
-    private final String password = "CS157DeeAein"; 
+
 
     public User getUserIfExist(int accountID){
-        String sql_query = "SELECT * FROM Users WHERE Account_ID = ?";
+        String sql_query = "SELECT U.Account_ID, U.Department_ID, U.Phone_Number, ACC.Username, ACC.Pwd_Hashed FROM Users U JOIN Accounts ACC ON U.Account_ID = ACC.Account_ID WHERE U.Account_ID = ?";
         List<User> resultUser = executeQuery(sql_query, accountID);
         
         if(resultUser.isEmpty()){
@@ -24,13 +23,16 @@ public class UserDAO {
         return resultUser.get(0); 
     }
 
+    public boolean insertUser(int accountID, int departmentID, String phNum){
+        String sql_query = "INSERT INTO Users (Account_ID, Department_ID, Phone_Number) VALUES (? ,? ,?)";
+        return executeInsertion(sql_query, accountID, departmentID, phNum);
+    }
+
     private List<User> executeQuery(String sql_query, Object... params){ 
         List<User> users = new ArrayList<>(); 
 
         try{
-            Class.forName("com.mysql.cj.jdbc.Driver"); 
-
-            Connection con = DriverManager.getConnection(url, user, password); //create connection  *****
+            Connection con = DBConnection.getConnection(); //create connection  *****
             PreparedStatement stmt = con.prepareStatement(sql_query); //SQL Execution //sends SQL *****
 
             for(int i = 0; i < params.length; i++){
@@ -42,9 +44,10 @@ public class UserDAO {
             while(rs.next()){
                 int accountID = rs.getInt("Account_ID");
                 int departmentID = rs.getInt("Department_ID");
-                String username = rs.getString("Username");
-                String pwdHashed = rs.getString("Pwd_hashed");
                 String phNum = rs.getString("Phone_Number");
+
+                String username = rs.getString("Username");
+                String pwdHashed = rs.getString("Pwd_Hashed");
 
 
                 users.add(new User(accountID, username, pwdHashed, departmentID, phNum));
@@ -59,5 +62,30 @@ public class UserDAO {
         }
         return  users;
     } //end of executeQuery()
+
+    private boolean executeInsertion(String sql_query, Object ... params){
+        boolean succssfulInsertion = false;
+
+        try{
+            Connection con = DBConnection.getConnection(); 
+            PreparedStatement stmt = con.prepareStatement(sql_query); //SQL Execution
+
+            for(int i = 0; i < params.length; i++){
+                stmt.setObject(i+1, params[i]);
+            }
+
+            int rowsAffected = stmt.executeUpdate(); //1 = 1 row inserted, 0 = insertion fails
+
+            if(rowsAffected > 0){ //successfully inserted
+                succssfulInsertion = true;
+            }
+
+            stmt.close();  //clean up ***** below
+            con.close(); 
+        } catch (Exception e){
+            e.printStackTrace(); 
+        }
+        return succssfulInsertion; 
+    }
 
 }
