de60edd885c174542db561eadcd2119577020263
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-25 16:50:40 -0700

    Finish purchaseOrderServlet doPost method that let users submit purchaseOrders

--- src/main/java/com/medicalims/servlet/PurchaseOrderServlet.java
+++ src/main/java/com/medicalims/servlet/PurchaseOrderServlet.java
@@ -5,7 +5,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
-import com.medicalims.model.OrderStatus;
 import com.medicalims.model.User;
 import com.medicalims.database.PurchaseOrderDAO;
 
@@ -34,7 +33,7 @@ public class PurchaseOrderServlet extends HttpServlet{
             return;
         }
 
-        //load items here to let the user pick from dropdown menu and forward it
+        //load items here to let the user pick from dropdown menu and forward it (LATER)
         //use request.getRequestDispatcher("/jsp/user-request-purchaseOrder.jsp").forward(request, response); 
     
     }
@@ -56,9 +55,67 @@ public class PurchaseOrderServlet extends HttpServlet{
         }
 
         //get form submission
-        //call PurchaseOrderDAO and insert a new purchase order using insertPurchaseOrder()
+        String itemReferenceNumString = request.getParameter("itemReferenceNum");
+        String qtyString = request.getParameter("quantity");
+        String message = request.getParameter("message");
 
-        //reload the items again (because if we don't set it after POST, dropdown will break)
+        if (itemReferenceNumString == null || qtyString == null || message == null){
+            request.setAttribute("error", "Input cannot be null!");
+            //set items again here!!
+            request.getRequestDispatcher("/pages/user-request-purchaseOrder.jsp").forward(request, response);
+            return;
+        }
+
+        itemReferenceNumString = itemReferenceNumString.trim();
+        qtyString = qtyString.trim();
+        message = message.trim(); 
+
+        if (itemReferenceNumString.isEmpty() || qtyString.isEmpty() || message.isEmpty()){
+            request.setAttribute("error", "Input cannot be empty!");
+            //set items again here!! (LATER)
+            request.getRequestDispatcher("/pages/user-request-purchaseOrder.jsp").forward(request, response);
+            return;
+        }
+
+        int itemReferenceNum;
+        int qty;
+
+        try{
+            itemReferenceNum= Integer.parseInt(itemReferenceNumString);
+            qty = Integer.parseInt(qtyString);
+        }catch (NumberFormatException e) {
+            request.setAttribute("error", "Item reference number and quantity must be integers");
+            //set items here again!!! (LATER)
+            request.getRequestDispatcher("/pages/user-request-purchaseOrder.jsp").forward(request, response);
+            return;
+        }
+
+        //check if the quantity is valid
+        if(qty <=0){
+            request.setAttribute("error", "Quantity must be greater than 0!");
+            //set items here again!!! (LATER)
+            request.getRequestDispatcher("/pages/user-request-purchaseOrder.jsp").forward(request, response);
+            return;
+        }
+
+        //check if item exists in the inventory using InventoryDAO  (LATER)
+
+       
+
+        //call PurchaseOrderDAO and insert a new purchase order using insertPurchaseOrder()
+        PurchaseOrderDAO purchaseOrderDAO = new PurchaseOrderDAO();
+        boolean inserted = purchaseOrderDAO.insertPurchaseOrder(itemReferenceNum, message, qty, user.getAccountID());
+
+        if (!inserted){
+            request.setAttribute("error", "Error submitting the request, Please try again!");
+            //set items again here!!
+            request.getRequestDispatcher("/pages/user-request-purchaseOrder.jsp").forward(request, response);
+            return;
+        }
+
+
+        response.sendRedirect(request.getContextPath() + "/pages/user-dashboard.jsp");
         //POST -> insert -> REDIRECT so that user can't do duplicate insert 
+
     }
 }
