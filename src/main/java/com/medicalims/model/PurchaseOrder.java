c68e0298a248a9c3251182ee8e2811f947a9d394
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-24 23:22:56 -0700

    Change the type of approvedBy from String to integer to recored the accountID of admin who approves the purchase request

--- src/main/java/com/medicalims/model/PurchaseOrder.java
+++ src/main/java/com/medicalims/model/PurchaseOrder.java
@@ -4,7 +4,7 @@ public class PurchaseOrder {
     private int orderID; 
     private int itemReferenceNum; 
     private OrderStatus status; 
-    private String approvedBy;
+    private int approvedBy;
 
     private int userID;
     private String message;
@@ -14,7 +14,7 @@ public class PurchaseOrder {
         this.orderID = 0;
         this.itemReferenceNum = 0;
         this.status = OrderStatus.PENDING;
-        this.approvedBy = "";
+        this.approvedBy = 0;
 
         this.userID = 0;
         this.message = "";
@@ -22,7 +22,7 @@ public class PurchaseOrder {
 
     }
 
-    public PurchaseOrder(int orderID, int itemReferenceNum, OrderStatus status, String approvedBy, int userID, String message, int qty) {
+    public PurchaseOrder(int orderID, int itemReferenceNum, OrderStatus status, int approvedBy, int userID, String message, int qty) {
         this.orderID = orderID;
         this.itemReferenceNum = itemReferenceNum;
         this.status = status;
@@ -36,7 +36,7 @@ public class PurchaseOrder {
     public void setOrderID(int orderID) { this.orderID = orderID; }
     public void setItemReferenceNum(int itemReferenceNum) { this.itemReferenceNum = itemReferenceNum; }
     public void setStatus(OrderStatus status) { this.status = status; }
-    public void setApprovedBy(String approvedBy) { this.approvedBy = approvedBy; }
+    public void setApprovedBy(int approvedBy) { this.approvedBy = approvedBy; }
     public void setUserID(int userID){this.userID=userID;}
     public void setMessage(String message){this.message=message;}
     public void setQty(int qty){this.qty=qty;}
@@ -44,7 +44,7 @@ public class PurchaseOrder {
     public int getOrderID() { return this.orderID; }
     public int getItemReferenceNum() { return this.itemReferenceNum; }
     public OrderStatus getStatus() { return this.status; }
-    public String getApprovedBy() { return this.approvedBy; }
+    public int getApprovedBy() { return this.approvedBy; }
     public int getUserID(){return userID;}
     public String getMessage(){return message;}
     public int getQty(){return qty;}
