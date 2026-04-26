c8da39f483749745935ce531795ad0a3b18e832f
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-25 19:31:31 -0700

    Draft UserInventoryDAO

---
+++ src/main/java/com/medicalims/database/UserInventoryDAO.java
@@ -0,0 +1,22 @@
+package com.medicalims.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.medicalims.util.DBConnection;
+import com.medicalims.model.Item;
+import com.medicalims.model.Inventory;
+
+public class UserInventoryDAO {
+    //User will view all the items in the inventory when he first click "View the inventory"
+    //User can sort the items with specified locations
+    //User can see the items with lot number 
+    //Users will see the items with the nearest expiration date first
+
+    //User will be able to withdraw items //a method that first check whether the item 
+
+    
+}
