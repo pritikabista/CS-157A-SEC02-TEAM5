e48ec4bb1258b10f741615567b96319fd6275595
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-25 19:16:40 -0700

    Matches Item and Inventory Model to the respective  Schema

--- src/main/java/com/medicalims/model/Inventory.java
+++ src/main/java/com/medicalims/model/Inventory.java
@@ -2,61 +2,27 @@ package com.medicalims.model;
 
 public class Inventory {
 
-    private int itemReferenceNumber;   
     private int locationID;
-    private String lotNumber;          
-    private int quantity;
-    private String itemName;           
-    private String categoryName;       
-    private String expirationDate;     
+    private int itemReferenceNumber;
+    private int stock;
 
     public Inventory() {
-        this.itemReferenceNumber = 0;
         this.locationID = 0;
-        this.lotNumber = "";
-        this.quantity = 0;
-        this.itemName = "";
-        this.categoryName = "";
-        this.expirationDate = "";
+        this.itemReferenceNumber = 0;
+        this.stock = 0;
     }
 
-    public Inventory(int itemReferenceNumber, String itemName, String categoryName,
-                     String lotNumber, String expirationDate, int quantity, int locationID) {
-
-        this.itemReferenceNumber = itemReferenceNumber;
-        this.itemName = itemName;
-        this.categoryName = categoryName;
-        this.lotNumber = lotNumber;
-        this.expirationDate = expirationDate;
-        this.quantity = quantity;
+    public Inventory(int locationID, int itemReferenceNumber, int stock) {
         this.locationID = locationID;
+        this.itemReferenceNumber = itemReferenceNumber;
+        this.stock = stock;
     }
 
-    public int getItemReferenceNumber() {
-        return itemReferenceNumber;
-    }
+    public int getLocationID() { return locationID; }
+    public int getItemReferenceNumber() { return itemReferenceNumber; }
+    public int getStock() { return stock; }
 
-    public String getItemName() {
-        return itemName;
-    }
-
-    public String getCategoryName() {
-        return categoryName;
-    }
-
-    public String getLotNumber() {
-        return lotNumber;
-    }
-
-    public String getExpirationDate() {
-        return expirationDate;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public int getLocationID() {
-        return locationID;
-    }
+    public void setLocationID(int locationID) { this.locationID = locationID; }
+    public void setItemReferenceNumber(int itemReferenceNumber) { this.itemReferenceNumber = itemReferenceNumber; }
+    public void setStock(int stock) { this.stock = stock; }
 }
--- src/main/java/com/medicalims/model/Item.java
+++ src/main/java/com/medicalims/model/Item.java
@@ -1,41 +1,42 @@
 package com.medicalims.model;
 
+import java.sql.Date;
+
 public class Item {
 
     private int itemReferenceNumber;
     private int categoryID;
+    private int lotNumber;
     private String itemName;
-    private String expirationDate;
+    private Date expirationDate;
 
     public Item() {
         this.itemReferenceNumber = 0;
         this.categoryID = 0;
+        this.lotNumber = 0;
         this.itemName = "";
-        this.expirationDate = "";
+        this.expirationDate = null;
     }
 
     public Item(int itemReferenceNumber, int categoryID,
-                String itemName, String expirationDate) {
+                int lotNumber, String itemName, Date expirationDate) {
 
         this.itemReferenceNumber = itemReferenceNumber;
         this.categoryID = categoryID;
+        this.lotNumber = lotNumber;
         this.itemName = itemName;
         this.expirationDate = expirationDate;
     }
 
-    public int getItemReferenceNumber() {
-        return itemReferenceNumber;
-    }
+    public int getItemReferenceNumber() { return itemReferenceNumber; }
+    public int getCategoryID() { return categoryID; }
+    public int getLotNumber() { return lotNumber; }
+    public String getItemName() { return itemName; }
+    public Date getExpirationDate() { return expirationDate; }
 
-    public int getCategoryID() {
-        return categoryID;
-    }
-
-    public String getItemName() {
-        return itemName;
-    }
-
-    public String getExpirationDate() {
-        return expirationDate;
-    }
+    public void setItemReferenceNumber(int itemReferenceNumber) { this.itemReferenceNumber = itemReferenceNumber; }
+    public void setCategoryID(int categoryID) { this.categoryID = categoryID; }
+    public void setLotNumber(int lotNumber) { this.lotNumber = lotNumber; }
+    public void setItemName(String itemName) { this.itemName = itemName; }
+    public void setExpirationDate(Date expirationDate) { this.expirationDate = expirationDate; }
 }
