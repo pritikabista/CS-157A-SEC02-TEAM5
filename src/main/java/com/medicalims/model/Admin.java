5e24cf58322968520ab7052f864ec2f59a8a544e
Dee <aeinhmu.naychi@sjsu.edu>
2026-04-26 16:59:14 -0700

    Makes office number a string

--- src/main/java/com/medicalims/model/Admin.java
+++ src/main/java/com/medicalims/model/Admin.java
@@ -2,23 +2,23 @@ package com.medicalims.model;
 
 public class Admin extends Account{
     private int departmentID;
-    private int officeNum; 
+    private String officeNum; 
 
     public Admin(){
         super();
         this.departmentID = 0;
-        this.officeNum = 0;
+        this.officeNum = "";
     }
 
-    public Admin(int accountID, String username, String pwdHashed, int departmentID, int officeNum){
+    public Admin(int accountID, String username, String pwdHashed, int departmentID, String officeNum){
         super(accountID, username, pwdHashed);
         this.departmentID = departmentID;
         this.officeNum = officeNum;
     }
 
     void setDepartmentID(int departmentID) { this.departmentID = departmentID; }
-    void setOfficeNum(int officeNum) { this.officeNum = officeNum; }
+    void setOfficeNum(String officeNum) { this.officeNum = officeNum; }
 
     int getDepartmentID() { return this.departmentID; }
-    int getOfficeNum() { return this.officeNum; }
+    String getOfficeNum() { return this.officeNum; }
 }
